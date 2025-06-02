8a2c87728108c71773cb15b47b0e4e60f88e374c
Hao-10 <gan64292008@gmail.com>
2025-06-02 16:49:28 +0800

    chore: 設定 Vite rollupOptions 增加 fallback 解決 GitHub Pages 404 問題

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/coffee-shop-web/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // 這裡新增一份 404.html，內容會跟 index.html 一樣
+        fallback: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 })
